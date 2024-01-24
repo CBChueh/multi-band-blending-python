e571de7cd0188b436754084ea416041ca6a4da04
CBChueh <f06941077@ntu.edu.tw>
2024-01-24 09:57:08 +0800

    minor changes

--- multi_band_blending.py
+++ multi_band_blending.py
@@ -7,7 +7,7 @@ import argparse
 
 def preprocess(img1, img2, overlap_w, flag_half, need_mask=False):
     if img1.shape[0] != img2.shape[0]:
-        print ("error: image dimension error")
+        print ("error: image height dimension error")
         sys.exit()
     if overlap_w > img1.shape[1] or overlap_w > img2.shape[1]:
         print ("error: overlapped area too large")
@@ -17,14 +17,14 @@ def preprocess(img1, img2, overlap_w, flag_half, need_mask=False):
     w2 = img2.shape[1]
 
     if flag_half:
+        # combine half of each image
         shape = np.array(img1.shape)
         shape[1] = w1 // 2 + w2 // 2
 
         subA = np.zeros(shape)
         subA[:, :w1 // 2 + overlap_w // 2] = img1[:, :w1 // 2 + overlap_w // 2]
         subB = np.zeros(shape)
-        subB[:, w1 // 2 - overlap_w // 2:] = img2[:,
-                                                w2 - (w2 // 2 + overlap_w // 2):]
+        subB[:, w1 // 2 - overlap_w // 2:] = img2[:, w2 - (w2 // 2 + overlap_w // 2):]
         if need_mask:
             mask = np.zeros(shape)
             mask[:, :w1 // 2] = 1
@@ -110,6 +110,40 @@ def multi_band_blending(img1, img2, mask, overlap_w, leveln=None, flag_half=Fals
 
     return result.astype(np.uint8)
 
+def multi_band_blending_var(img1, img2, mask, leveln=None, flag_half=False, need_mask=False):
+    # assume img1 and img2 are same size
+    
+    if overlap_w < 0:
+        print ("error: overlap_w should be a positive integer")
+        sys.exit()
+
+    if need_mask:  # no input mask so create one
+        subA, subB, mask = preprocess(img1, img2, overlap_w, flag_half, True)
+    else:  # have input mask
+        subA, subB, _ = preprocess(img1, img2, overlap_w, flag_half)
+
+    max_leveln = int(np.floor(np.log2(min(img1.shape[0], img1.shape[1],
+                                          img2.shape[0], img2.shape[1]))))
+    if leveln is None:
+        leveln = max_leveln
+    if leveln < 1 or leveln > max_leveln:
+        print ("warning: inappropriate number of leveln")
+        leveln = max_leveln
+
+    # Get Gaussian pyramid and Laplacian pyramid
+    MP = GaussianPyramid(mask, leveln)
+    LPA = LaplacianPyramid(subA, leveln)
+    LPB = LaplacianPyramid(subB, leveln)
+
+    # Blend two Laplacian pyramidspass
+    blended = blend_pyramid(LPA, LPB, MP)
+
+    # Reconstruction process
+    result = reconstruct(blended)
+    result[result > 255] = 255
+    result[result < 0] = 0
+
+    return result.astype(np.uint8)
 
 if __name__ == '__main__':
     # construct the argument parse and parse the arguments
